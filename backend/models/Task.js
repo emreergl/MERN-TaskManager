ddb67505d62a33b464c6ea94cc176d7f68c59862
Ogrenci <ogrenci@universite.edu.tr>
2026-05-26 19:13:52 +0300

    MongoDB semalari ve modelleri (User, Category, Task) olusturuldu

---
+++ backend/models/Task.js
@@ -0,0 +1,9 @@
+﻿const mongoose = require('mongoose');
+const TaskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    status: { type: String, enum: ['todo', 'doing', 'done'], default: 'todo' },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+module.exports = mongoose.model('Task', TaskSchema);
